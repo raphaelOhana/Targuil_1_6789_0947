5f2f5cc62c4a135ffc4feb62110b04ffe344fa7a
reouven smadja <rusmadja@g.jct.ac.il>
2020-10-27 23:03:58 +0200

    checker et faire des vrai fonction

--- src/Comunity_Member.kt
+++ src/Comunity_Member.kt
@@ -3,12 +3,10 @@ import java.util.*
 abstract class Comunity_Member (ID:String , gender:Gender,  name: String,  adresse:String,  birthday: Date,  torah_hours: Int,  work_hour : Int, Salary:Int) : Obligation{
     abstract fun print_details()
 
-    var ID : String = ID
+    val ID : String = ID
         get() =  field
-        private set(value) {field = value}
-    var gender : Gender  = gender
+    val gender : Gender  = gender
         get() =  field
-        private set(value) {field = value}
     var name : String = name
         get() =  field
         private set(value) {field = value}
